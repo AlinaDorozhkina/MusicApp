c6739b18e318ee1185e241ca2fd8678b9ebe51b6
app-droid <alinadorozhkina91@mail.ru>
2021-06-29 21:49:42 +0300

    создала метод для изменеия бекграунда треков-лидеров

--- app/src/main/java/ru/alinadorozhkina/helper/Keys.kt
+++ app/src/main/java/ru/alinadorozhkina/helper/Keys.kt
@@ -1,3 +1,23 @@
 package ru.alinadorozhkina.helper
 
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.TypedValue
+import ru.alinadorozhkina.musicapp.R
+
+
 var SHARED_THEME = "pink"
+
+fun getColorRes(context: Context, position: Int): Int = when (position) {
+    1 -> context.resources.getColor(R.color.gold)
+    2 -> context.resources.getColor(R.color.silver)
+    3 -> context.resources.getColor(R.color.bronze)
+    else -> context.resources.getColor(R.color.grey_dark)
+}
+
+
+
+interface BackButtonListener {
+    fun backPressed(): Boolean
+}
+
